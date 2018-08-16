ba44a5ef6a4faafa47fb3b73a4e05615f73a78a2
erquier <ernest324@gmail.com>
2018-08-16 16:57:07 -0400

    creating starbase proyect using vuejs

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h3>{{title}}</h3>
+    <div class="row">
+      <div class="col s1">
+        <Character v-for="id in initial_ids" v-bind:id="id"/>
+
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Character from './components/Character'
+  export default{
+    name:'app',
+    data(){
+      return{
+        title: 'Generate Your Team',
+        initial_ids:[1,13,14]
+      }
+    },
+    components:{
+      Character
+    }
+  }
+</script>
---
+++ src/components/Character.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="">
+    {{character}}
+  </div>
+</template>
+
+<script>
+  export default{
+    props:['id'],
+    data(){
+      return{
+        character:{}
+      }
+    },
+    methods:{
+      fetchCharacter(id) {
+        fetch(`https://swapi.co/api/people/${id}`,{
+          method: 'GET'
+        })
+        .then(response => response.json())
+        .then(json => this.character=json)
+      }
+    },
+    created(){
+      this.fetchCharacter(this.id)
+    }
+  }
+</script>
